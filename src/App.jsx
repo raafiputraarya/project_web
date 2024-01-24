1486130f4a398f3db27918bfa0d9423998936c43
raafiputraarya <raafiputra2407@gmail.com>
2024-01-24 16:47:33 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>selamat datang di website kami</p>
     </>
   )
 }
