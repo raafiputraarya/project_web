f4849a0b9df8296431a3b163b7a2011e1896b575
raafiputraarya <raafiputra2407@gmail.com>
2024-02-06 17:24:32 +0700

    test bug

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,11 +1,7 @@
 function Service (){
     return(
-        <>
-<<<<<<< HEAD
-        
-=======
+        <>      
             <h2>Ini Adalah Menu Sevice</h2>
->>>>>>> d7989c52075a3bfe21090c89302c0d4d03ea0ba3
         </>
     );
 }
