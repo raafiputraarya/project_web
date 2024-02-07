6707395c6540d656e32b2beee8919b802b9f7df4
Rezardy <imrezerd@gmail.com>
2024-02-07 14:14:47 +0700

    About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function About (){
     return(
         <>
-        <h2>Ini Adalah Menu About</h2>
+        <Container>
+            <Row><h1 className="text-center">About</h1></Row>
+            <Row>
+                <Col>
+                    <img src="img/logo.jpeg" alt="" className="img-fluid"/>
+                </Col>
+                <Col>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+sed do eiusm sed do eiusmod tempor incididunt ut labore
+et dolore magna aliqua.Lorem ipsum dolor sit amet,
+consectetur adipisicing elit, sed do eiusm sed do eiusmod
+tempor incididunt ut labore et dolore magna
+aliqua.Lorem ipsum dolor sit amet, consectetur
+adipisicing elit, sed do </p>
+                    <p>
+                    eiusm sed do eiusmod tempor incididunt ut labore et
+dolore magna aliqua.Lorem ipsum dolor sit amet,
+consectetur
+
+ adipisicing elit, sed do eiusm sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.Lorem ipsum
+dolor sit amet, consectetur adipisicing elit, sed do eiusm
+sed do eiusmod tempor incididunt ut labore et dolore
+magna aliqua.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
         </>
     );
 }
