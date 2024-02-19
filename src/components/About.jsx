4e98db35ccf2646e75cacca9acdca5f2abbf089a
raafiputraarya <raafiputra2407@gmail.com>
2024-02-19 16:00:22 +0700

    bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,12 @@ function About (){
         <Container className="amazingAbout px-5" id="About" fluid>
             <Row><h1 className="title text-center mb-4 mt-5">ABOUT</h1></Row>
             <Row>
-<<<<<<< HEAD
+
                 <Col lg={6} md={12} className="d-flex mb-5 mt-3">
                     <img src="/img/logo.jpeg" alt="" className="img-fluid mx-auto mt-1 mb-auto logoAbout"/>
-=======
+                </Col>
                 <Col lg={6} md={12} className="d-flex">
                     <img src="img/logo.jpeg" alt="" className="img-fluid mx-auto mt-1 mb-auto logoa"/>
->>>>>>> abde5941832b73308d0079e08ca1e9a9b24bf0d3
                 </Col>
                 <Col lg={6} md={12} className='mb-5 mt-3'>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
