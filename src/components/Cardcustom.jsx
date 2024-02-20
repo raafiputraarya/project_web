5d1b7ba3a6bbf1738cc533a4c53bdb4cc60e0c3e
Rezardy <imrezerd@gmail.com>
2024-02-20 12:56:32 +0700

    aos 3

--- src/components/Cardcustom.jsx
+++ src/components/Cardcustom.jsx
@@ -5,6 +5,7 @@ import './Cardcustom.css'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row' ;
 import Col from 'react-bootstrap/Col' ;
+import 'aos/dist/aos.css';
 
 function Cardcustom(){
     return (
