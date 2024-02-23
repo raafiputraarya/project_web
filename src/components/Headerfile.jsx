0e0a4e7725e6b35709aaede2b0068fe5940f2c5b
Rezardy <imrezerd@gmail.com>
2024-02-23 15:29:31 +0700

    headerfix

--- src/components/Headerfile.jsx
+++ src/components/Headerfile.jsx
@@ -2,14 +2,15 @@ import { Container } from "react-bootstrap"
 import "./headerfile.css"
 import Row from 'react-bootstrap/Row' 
 import Button from 'react-bootstrap/Button'
+import 'aos/dist/aos.css';
 
 function Headerfile (){
     return(
     <div className="header" id='Header'>
             <Container className="home">
                 <Row>
-                    <div className="sectionleft col-md-6">
-                        <h2 className='title1' >HELLO!</h2>
+                    <div className="sectionleft col-md-6" data-aos="fade-right" data-aos-duration="1000">
+                        <h2 className='title1'>HELLO!</h2>
                         <h3 className='titlewelcome' >Welcome to</h3>
                         <h2 className='primarytitle' >Kabar6.com</h2>
                         <p className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore esse quo quaerat nulla provident molestiae possimus nisi quisquam molestias voluptatum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, quos.</p>
@@ -17,7 +18,7 @@ function Headerfile (){
                     </div>
 
                     <div className="sectionright col-md-2">
-                        <img src='./img/logo.jpeg' className="imageheader"></img>
+                        <img src='img/logo.jpeg' className="imageheader"></img>
                     </div>
                 </Row>
             </Container>
