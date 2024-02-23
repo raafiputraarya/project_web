4be64a092ffd426342fb732d7b016d20231beb37
Rezardy <imrezerd@gmail.com>
2024-02-23 15:31:52 +0700

    Tabs +placeholder

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
 import './About.css'
 function About (){
     return(
@@ -8,28 +10,84 @@ function About (){
         <Container className="amazingAbout px-5" id="About" fluid>
             <Row><h1 className="title text-center mb-4 mt-5">ABOUT</h1></Row>
             <Row>
-                <Col lg={6} md={12} className="d-flex">
+                <Col lg={6} md={12} className="d-flex mt-5 mb-5">
                     <img src="img/logo.jpeg" alt="" className="img-fluid mx-auto mt-1 mb-auto logoa"/>
                 </Col>
-                <Col lg={6} md={12} className='mb-5 mt-3'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusm sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua.Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit, sed do eiusm sed do eiusmod
-                    tempor incididunt ut labore et dolore magna
-                    aliqua.Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit, sed do </p>
-                    <p>
-                    eiusm sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua.Lorem ipsum dolor sit amet,
-                    consectetur
+                <Col lg={6} md={12} className=''>
+                    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                        <Row>
+                            <Col sm={3}>
+                            <Nav variant="pills" className="flex-column">
+                                <Nav.Item>
+                                <Nav.Link eventKey="first">PROFIL</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                <Nav.Link eventKey="second">The Rest</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            </Col>
+                            <Col sm={9}>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <p>
+                                    Kabar6 lahir Tahun 2011 di Bawah bendera CV Karya Multi Media, kemudian berubah nama di bawah naungan CV.KABAR6 MULTI MEDIA, pada Tahun 2016 tanggal 14 April status legalitasnya menjadi PT KABAR6 MULT I MEDIA. Kemudian mengalami akta Perubahan Tanggal 22 Maret 2017 Nomer. 
+                                    </p>
+                                    <p>
+                                    Meskipun berganti legalitas Poundernya hanya 1, yakni Almarhum Parluhutan Gultom, wartawan yang  mulai berkarir dari Harian Lampu Ijo, Pos Metro Jakarta, Koran Sore Sinar Harapan, dan  akhirnya menciptakan karya sendiri yakni kabar6.com, sekaligus pemilik media. Tahun 2021 beliau wafat diusia 47 Tahun karena sakit.
+                                    </p>
+                                    <p>
+                                    Kini media masih eksis diterukan pengurus yang juga tertera di dalam legalitas PT.Kabar6 Multi Media. Komisari Irsa Pitri Damanik, Direktur Sukardin SH.MH, dan Direkrur Operasional Yudi Wibowo. Kepada 3 personil inilah media ini dititipkan almarhum Parluhutan Gultan yang biasa disebut Bang Luhut
+                                    </p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <p>
+                                    Labar Belakang 
+Kita tahu, pesatnya pertumbuhan tekhnologi dewasa ini kian tak terbendung. 15 tahun lalu jaringan internet umumnya diakeses melalui komputer, namun sekarang tidak perlu lagi. Karena, mayoritas telepon seluler yang hadir ke masyarakat sudah dilengkapi dengan layanan internet.
+Tingginya kebutuhan masyarakat akan informasi serta pesatnya pertumbuhan tekhnologi dewasa ini, menjadi salah satu dasar bagi kami dalam wadah PT KABAR6 MULTIMEDIA dengan Akta Perubahan  22 Maret 2017 Nomer 08 dan Nomer AHU-0007182.AH.01.02.Tahun 2017 serta Surat Pengukuhan PKP:-1S30PKP/WP J.08/KP.0303/2017 hadir melalui www.kabar6.com, untuk menyajikan informasi berbasis dunia internet kehadapan masyarakat. Dan, kami siap menyajikan informasi langsung ke genggaman Anda.
+Terbukti sejak diluncurkan hingga sekarang, www.kabar6.com kiranya cukup mendapat apresiasi dari masyarakat Banten, khususnya Tangerang Raya. Terbukti, kabar6 sudah berusia 14 tahun, dan  kabar6.com kini sudah memiliki kunjungan hingga rata-rata mencapai 1.500.000 clikers perhari versi google analystic. 125-250 Ribu kunjungan per hari dari data administrator web kabar6.com
+Filosofi Kabar6 ini diambail dari kata yang enak didengar dan diucapkan untuk sebuat nama media online  Kabar dan 6 ini maksudnya untyuk membuka hari web ini memuncilkan 6 berita untuk pembacanya.
+Memasuki Tahun 2021-sampai saat ini PT.KABAR6 MULTI MEDIA juga menaungi Media Agribisnis ww.agrofarm.co.id. Berkolaborainya media Agrofarm tak lepas dari tata kelolah managemant yang rapi dan termasuk Perusahana Kena Pajak (PKP) yang memudahkan  untuk mejalin kerjasama dengan Pemerintah dan swasta.
+PEMBACA 
+Spesifikasi Pengunjung Kabar6.com
+ Jumlah pengunjung kabar6.com yang terdaftar di google analystic  2020  
+ sebanyak 100.000 IP address perhari, dengan total clikers  
+ mencapai 1.500.000 perhari.
+ 
+      Spesifikasi pengunjung terdiri dari:
+-	Kalangan Menengah Atas: 
+           Pejabat publik, Pengusaha, Aktivis (ormas) total 40 persen.
+-	Umum:
+	@ Pelajar, Pegawai Swasta, PNS & Guru total 40 persen
+	@ Penganggur (pencari kerja) total 15 persen
+-	Lain-lain:
+	@ Pengunjung dari luar negri total 5 persen. 
+Selayang Pandang 
+Media online (online media) adalah media massa yang tersaji secara online di situs web (website) internet.
+Media online adalah media massa ”generasi ketiga” setelah media cetak (printed media) –koran, tabloid, majalah, buku– dan media elektronik (electronic media) –radio, televisi, dan film/video.
+Media Online merupakan produk jurnalistik online. Jurnalistik online–disebut juga cyber journalisme– didefinisikan sebagai “pelaporan fakta atau peristiwa yang diproduksi dan didistribusikan melalui internet” (wikipedia).
+Secara teknis atau ”fisik”, media online yang juga disebut Digital Media adalah media berbasis telekomunikasi dan multimedia (komputer dan internet). Termasuk kategori media online adalah portal, website (situs web, termasuk blog), radio online, TV online, dan email.
+Karakteristik dan keunggulan media online dibandingkan ”media konvensional” (cetak/elektronik) antara lain:
+* Kapasitas luas –halaman web bisa menampung naskah sangat panjang
+* Pemuatan dan editing naskah bisa kapan saja dan di mana saja.
+* Jadwal terbit bisa kapan saja bisa, setiap saat.
+* Cepat, begitu di-upload langsung bisa diakses semua orang.
+* Menjangkau seluruh dunia yang memiliki akses internet.
+* Aktual, berisi info aktual karena kemudahan dan kecepatan penyajian.
+* Update, pembaruan informasi terus dan dapat dilakukan kapan saja.
+* Interaktif, dua arah, dan ”egaliter” dengan adanya fasilitas kolom komentar, chat room, polling, dsb.
+* Terdokumentasi, informasi tersimpan di ”bank data” (arsip) dan dapat ditemukan melalui ”link”, ”artikel terkait”, dan    
+   Fasilitas ”cari” (search).
+* Terhubung dengan sumber lain (hyperlink) yang berkaitan dengan informasi tersaji.
+Visi dan MISI
+Kabar6 merupakan media mainsterm yang lahir di Banten dengan motto Dari Banten untuk Indonesia. Menyajikan media nomer 1 yanng masih eksis sampai saat ini. Adapun sajian informasi yang diproduksi www.kabar6.com mulai dari berita seputar pemerintahan, politik, kriminal dan hukum. Kami juga menyajikan sejumlah kanal hiburan, bisnis, kuliner, sehat, fengshui  dan busyet dah. Kami percaya sajian informasi berbasis data dan fakta yang kami suguhkan bisa menjadi salah satu referensi, umumnya bagi warga Banten, dan khusunya bagi warga Tangerang Raya. 
+Media ini juga berfungsi sebagai kontrol sosial, dalam menyoroti kebijakan Pemerintah Daerah, dan juga mengutamakan kepentingan rakyat. Idialisme sebagai media yang independen tanpa keberpihakan pada politik, pemerintah, maupun kelompok-kelompok tertentu.  Dalam menyajikan informasih kabar6 memakai pedoman dan kode etik jurnalistik dan UU Pers. Media ini juga sudah terverifikasi faktual DEWAN PERS.  
 
-                    adipisicing elit, sed do eiusm sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.Lorem ipsum
-                    dolor sit amet, consectetur adipisicing elit, sed do eiusm
-                    sed do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua.
-                    </p>
+                                    </p>
+                                </Tab.Pane>
+                            </Tab.Content>
+                            </Col>
+                        </Row>
+                    </Tab.Container>
                 </Col>
             </Row>
         </Container>
