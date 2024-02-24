2d6672d6526cfc63b1f5084824cc7dbc4a27632c
raafiputraarya <raafiputra2407@gmail.com>
2024-02-24 10:53:43 +0700

    pengisian konten footer dan mengisi konten header yg ilang

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,33 +15,33 @@ function Footer() {
           </Col>
           <Col md={2} sm={6}>
             <b>Kabar6.com</b>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Kabar6 merupakan media mainsterm yang lahir di Banten dengan motto Dari Banten untuk Indonesia.</p>
           </Col>
           <Col md={2} sm={4}>
             <b>Important Links</b>
             <ul className="list-unstyled">
-              <li>Home</li>
-              <li>About</li>
-              <li>Services</li>
-              <li>Contact</li>
+              <li><a href='#Header'>Home</a></li>
+              <li><a href='#Services'>Services</a></li>
+              <li><a href='#About'>About</a></li>
+              <li><a href='#Contact'>Contact</a></li>
             </ul>
           </Col>
           <Col md={3} sm={4}>
             <b>Contact</b>
             <ul className="list-unstyled">
-              <li>Gmail</li>
-              <li>lorem ipsum</li>
-              <li>Phone number</li>
-              <li>Other Phone number</li>
+              <li><a href='https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=kabarenam6@gmail.com'>Gmail</a></li>
+              <li><a href='mailto:kabar6.com'>Yahoo</a></li>
+              <li><a href='https://api.whatsapp.com/send/?phone=%2B628158407736&text&type=phone_number&app_absent=0'>Phone number</a></li>
+              <li><a href='tel:021 2275 6598'>Other Phone number</a></li>
             </ul>
           </Col>
           <Col md={3} sm={4}>
             <b>Social Media</b>
             <ul className="list-unstyled">
-              <li>Youtube</li>
-              <li>Instagram</li>
-              <li>Twitter</li>
-              <li>Facebook</li>
+              <li><a href='https://www.youtube.com/results?search_query=kabarenam_official'>Youtube</a></li>
+              <li><a href='https://www.instagram.com/kabar6official/'>Instagram</a></li>
+              <li><a href='https://twitter.com/Kabar6com'>Twitter</a></li>
+              <li><a href='https://api.whatsapp.com/send/?phone=%2B628158407736&text&type=phone_number&app_absent=0'>Whatsapp</a></li>
             </ul>
           </Col>
         </Row>
--- src/components/Headerfile.jsx
+++ src/components/Headerfile.jsx
@@ -13,7 +13,7 @@ function Headerfile (){
                         <h2 className='title1'>HELLO!</h2>
                         <h3 className='titlewelcome' >Welcome to</h3>
                         <h2 className='primarytitle' >Kabar6.com</h2>
-                        <p className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore esse quo quaerat nulla provident molestiae possimus nisi quisquam molestias voluptatum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, quos.</p>
+                        <p className="subtitle"><b>Kabar6</b> merupakan media mainsterm yang lahir di Banten dengan motto Dari Banten untuk Indonesia. Menyajikan media nomer 1 yanng masih eksis sampai saat ini. Adapun sajian informasi yang diproduksi www.kabar6.com mulai dari berita seputar pemerintahan, politik, kriminal dan hukum.</p>
                         <Button className="Button" variant="warning" href='#Services' >Read now!</Button>
                     </div>
                     <div md={2} className="sectionright col-md-2"  data-aos="fade-left" data-aos-duration="2000">
